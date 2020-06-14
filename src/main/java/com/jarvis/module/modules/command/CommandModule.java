3c83c4956e2ff9bacbb59db3f7e88feffbc5f0d0
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2020-06-14 13:44:01 +0200

    Fixes #28: Find all commands automatically

--- src/main/java/com/jarvis/module/modules/command/CommandModule.java
+++ src/main/java/com/jarvis/module/modules/command/CommandModule.java
@@ -3,24 +3,16 @@ package com.jarvis.module.modules.command;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import com.jarvis.Jarvis;
 import com.jarvis.commands.Command;
-import com.jarvis.commands.commands.PrintCommandsCommand;
-import com.jarvis.commands.commands.PrintHistoryCommand;
-import com.jarvis.commands.jarvis.ShutdownCommand;
-import com.jarvis.commands.jobs.BackgroundCommand;
-import com.jarvis.commands.logging.ChangeLogLevelCommand;
-import com.jarvis.commands.settings.ChangeSettingCommand;
-import com.jarvis.commands.settings.PrintSettingCommand;
-import com.jarvis.commands.settings.ReloadSettingCommand;
-import com.jarvis.commands.settings.ResetSettingCommand;
-import com.jarvis.commands.settings.SaveSettingsCommand;
-import com.jarvis.commands.statistics.PrintStatisticCommand;
 import com.jarvis.events.JarvisShutdownEvent;
 import com.jarvis.module.Module;
 import com.jarvis.module.ModuleHandler;
 import com.jarvis.module.modules.logging.Logger;
+
+import org.reflections.Reflections;
 
 public class CommandModule extends Module {
 
@@ -37,21 +29,15 @@ public class CommandModule extends Module {
 		Logger.getRootLogger().trace("Loading commands");
 		commands = new ArrayList<>();
 
-		commands.add(new ShutdownCommand());
-		commands.add(new PrintHistoryCommand());
-
-		commands.add(new ChangeLogLevelCommand());
-
-		commands.add(new ChangeSettingCommand());
-		commands.add(new ResetSettingCommand());
-		commands.add(new PrintSettingCommand());
-		commands.add(new ReloadSettingCommand());
-		commands.add(new SaveSettingsCommand());
-
-		commands.add(new BackgroundCommand());
-
-		commands.add(new PrintStatisticCommand());
-		commands.add(new PrintCommandsCommand());
+		Reflections reflections = new Reflections("com.jarvis.commands");
+		Set<Class<? extends Command>> commandList = reflections.getSubTypesOf(Command.class);
+		commandList.forEach(command -> {
+			try {
+				commands.add(command.getConstructor().newInstance());
+			} catch (Exception e) {
+				Logger.getRootLogger().error("Failed to instantiate command");
+			}
+		});
 
 		Logger.getRootLogger().trace("Finished loading commands");
 	}
